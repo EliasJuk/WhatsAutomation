2a15ecd9b504fc06400047f4de75e7e5c1cfdfda
EliasJuk <eliasp.juk@hotmail.com>
2021-12-30 20:31:43 -0300

    enviar msg para cada contato na planilha

--- WhatsAutomation/app.py
+++ WhatsAutomation/app.py
@@ -2,18 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from urllib.parse import quote
 import time
+import pandas as pd
 
 driver = webdriver.Chrome()
 
-# ENTRAR NO SITE DO WHATSAPP
-driver.get('https://web.whatsapp.com/')
-time.sleep(4)
+# INICIA AUTOMATIZAÇÃO
+def iniciar():
+  planilha_contatos()
 
-# ENVIA MENSAGEM DE TEXTO
-nome = 'Elias'
-numero = '554200000000'
-texto = quote(f'olá {nome}, tudo bem com você')
-link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
-driver.get(link)
-time.sleep(4)
-driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]').send_keys(Keys.ENTER)
+# CARREGA A PLANILHA COM CONTATOS
+def planilha_contatos():
+  tabela = pd.read_excel('contatos.xlsx', sheet_name='contatos')
+  buscar_contatos(tabela)
+
+def buscar_contatos(tabela):
+  maxContatos = len(tabela.index)  
+  for x in range(maxContatos):
+    whatsapp(tabela.Nome[x], tabela.Numero[x])
+
+def whatsapp(nome, numero):
+  # ENTRAR NO SITE DO WHATSAPP
+  driver.get('https://web.whatsapp.com/')
+  time.sleep(4)
+
+  # ENVIA MENSAGEM DE TEXTO
+  texto = quote(f'olá {nome}, tudo bem com você')
+  link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
+  driver.get(link)
+  time.sleep(4)
+  enviar_msg = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]')
+  enviar_msg.send_keys(Keys.ENTER)
+  time.sleep(2)
+
+iniciar()
