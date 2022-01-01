6ae934708a5b8c5e6bebe4172d0eeb88834926bc
EliasJuk <eliasp.juk@hotmail.com>
2022-01-01 20:15:43 -0300

    update run.bat

--- WhatsAutomation/run.bat
+++ WhatsAutomation/run.bat
@@ -1,5 +1,17 @@
-cd venv/Scripts/
-call activate.bat
-cd ../../
+SET var=%cd%
 
-call python app.py
+IF EXIST "%var%\venv" (
+	cd venv/Scripts/
+	call activate.bat
+	cd ../../
+  call python app.py
+
+) ELSE (
+  call python -m venv venv
+	cd venv/Scripts/
+	call activate.bat
+	cd ../../
+	call python -m pip install --upgrade pip
+	pip install -r "%var%\requirements.txt"
+  call python app.py
+)
