9d7a38ddb11d94f9ce3077089b04bbf93f062eea
EliasJuk <eliasp.juk@hotmail.com>
2022-01-01 22:26:28 -0300

    componentes

--- WhatsAutomation/main.py
+++ WhatsAutomation/main.py
@@ -6,6 +6,10 @@ class Aplication:
     self.root = Tk()
     self.window()
     self.frames()
+    self.labels()
+    self.textbox()
+    self.buttons()
+    self.checkbox()
     self.automation()
     self.root.mainloop()
   
@@ -29,6 +33,22 @@ class Aplication:
     # IMG
     self.frame_3 = Frame(self.root, bg='#45475b')
     self.frame_3.place(relx=.02, rely=.58, relwidth=.96, relheight=.4)
+
+  def labels(self):
+    self.label_1 = Label(self.frame_1, text='Mensagem a ser enviada:', bg='#45475b', fg='#D9D9D9')
+    self.label_1.place(relx=.3, rely=.03)
+
+  def textbox(self):
+    self.textbox_1 = Text(self.frame_1, height=9, width=37, bg='#d4d4d4', pady=10, padx=10, font='sans-serif')
+    self.textbox_1.place(relx=.04, rely=.16, relwidth=.92, relheight=.74)
+
+  def buttons(self):
+    self.button_1 = Button(self.frame_2, text='Open file', bg='#d4d4d4')
+    self.button_1.place(relx=.26, rely=.3, relwidth=.2, relheight=.4)
+
+  def checkbox(self):
+    self.checkbox_1 = Checkbutton(self.frame_2, text='Imagem', bg='#e1e1e1', onvalue=1, offvalue=0)
+    self.checkbox_1.place(relx=.50, rely=.3, relheight=.4)
 
   def automation(self):
     self.root_path = os.getcwd()
